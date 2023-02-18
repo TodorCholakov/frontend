03272f393eacbbff2cd58f2a15923dcca9388cf8
Todor Cholakov <t.s.cholakov@gmail.com>
2023-02-18 16:17:49 +0200

    kk

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,18 +1,122 @@
-import Link from "next/link"
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
 
-const NavBar = () => {
+const Nav = () => {
+  const Hover = () => {};
+
   return (
-    <div>
-    <ul>
-        <li>
-            <Link   href="/">Home</Link>
-            <Link href="/gallery">Gallery</Link>
-            <Link href="/requestAnImage">request an image</Link>
-            <Link href="/contact">Contact</Link>
-        </li>
-    </ul>
-    </div>
-  )
-}
+    <NavWrapper>
+      <UlList onMouseOver={Hover}>
+        <ListElement>
+          <Link href="#">Menu</Link>
+        </ListElement>
+        <ListElement>
+          <Link href="/">Home</Link>
+        </ListElement>
+        <ListElement>
+          <Link href="/gallery">Photography</Link>
+        </ListElement>
+        <ListElement>
+          <Link href="/store">Request an image</Link>
+        </ListElement>
+        <ListElement>
+          <Link href="/contact">Contact</Link>
+        </ListElement>
+      </UlList>
+    </NavWrapper>
+  );
+};
 
-export default NavBar
+const NavWrapper = styled.div`
+  z-index: 9999;
+  height: 40px;
+  display: flex;
+  display: -webkit-flex;
+  -webkit-justify-content: space-between;
+  flex-direction: column;
+  justify-content: space-between;
+  width:100vw;
+
+  @media (max-width: 768px) {
+    transition: 500ms;
+    transition-timing-function: ease-in;
+    opacity: 1;
+    overflow: hidden;
+    background-color: #000000;
+    &:hover {
+      transition-timing-function: ease-in;
+      z-index: 9999;
+      transition: 500ms;
+      height: 260px;
+    }
+  }
+`;
+const ListElement = styled.li`
+  display: flex;
+  color: #fff;
+  fill: #fff;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-size: 13px;
+  font-weight: 200;
+  letter-spacing: 1px;
+  &:nth-child(1) {
+    display: none;
+  }
+  @media (max-width: 768px) {
+    background-color: black;
+    padding: 10px;
+    justify-content: center;
+    width: 100%;
+
+    &:nth-child(1) {
+      text-align: center;
+      display: inline;
+      margin-top:-5px;
+    }
+  }
+`;
+const UlList = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  background-color: black;
+  opacity: 0.7;
+  font-family: 'Montserrat', sans-serif;
+  justify-content: right;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    opacity: 1;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+    padding: 5px 10px;
+    position: relative;
+  }
+  a:after {
+    background: none repeat scroll 0 0 transparent;
+    bottom: 0;
+    content: "";
+    display: block;
+    height: 2px;
+    left: 50%;
+    position: absolute;
+    background: #fff;
+    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    width: 0;
+  }
+  a:hover:after {
+    width: 100%;
+    left: 0;
+  }
+`;
+
+export default Nav;
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,8 +1,182 @@
 
-const gallery = () => {
-  return (
-    <div>gallerz sdfasdf adsasdf</div>
-  )
-}
+import styled from "styled-components";
+import Image from "next/image";
+import PhotographyBackground from 'public/images/GalleryPage/4.jpg'
+import HeaderPageImage1 from 'public/images/GalleryPage/1.webp'
+import TheMountain from 'public/images/GalleryPage/3.webp'
+import Air from 'public/images/GalleryPage/2.webp'
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
+import NavBar from "../components/NavBar"
 
-export default gallery
+const GalleryPage = () => {
+  const moveRight1 = {
+    hidden: { x: -1500 },
+    show: {
+      x: 0,
+      transition: { duration: 1, ease: "easeOut" },
+    },
+  };
+
+  return (
+    <Wrapper>
+       <Image  className='object-cover z-0 'fill src={PhotographyBackground} />
+    <SubWrapper>
+       <NavBar />
+    </SubWrapper>
+
+  <div variants={moveRight1} initial="hidden" animate="show" className="flex justify-center flex-row content-center" >  
+
+
+      <Link href={`/wildlife-gallery`}>
+      <Card sx={{ maxWidth: 345 }} className="m-5">
+      <CardActionArea style={{backgroundColor: "white"}}>
+      <Image src={HeaderPageImage1}  />
+        <CardContent>
+        <Title>WILDLIFE</Title>
+        <HR />
+            <SubTitle>
+              Hidden deep into the mountain, out of sight, they live an amazing
+              life
+            </SubTitle>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+</Link>
+
+
+    <Link href={`/wildlife-gallery`}>
+    <Card sx={{ maxWidth: 345 }} className="m-5">
+      <CardActionArea style={{backgroundColor: "white"}}>
+      <Image src={TheMountain}  />
+        <CardContent>
+        <Title>THE MOUNTAIN</Title>
+            <HR />
+            <SubTitle>The higher in mountain, the lighter is soul</SubTitle>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Link>  
+    
+
+       <Link href={`/air-gallery`}>
+    <Card sx={{ maxWidth: 345 }} className="m-5">
+      <CardActionArea style={{backgroundColor: "white"}}>
+      <Image src={Air}  />
+        <CardContent>
+        <Title>AIR</Title>
+            <HR />
+            <SubTitle>Fly high to feel the silence</SubTitle>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Link>  
+    
+
+   </div>
+    </Wrapper>
+  );
+};
+
+const HR = styled.div`
+  border-bottom: 1px solid gray;
+  margin: 3px;
+  opacity: 0.7;
+  width: 100%;
+`;
+const Title = styled(motion.h2)`
+  font-family: "Montserrat", sans-serif;
+  font-size: 18px;
+  font-weight: 300;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+const SubTitle = styled(motion.p)`
+  font-family: "Montserrat", sans-serif;
+  font-size: 14px;
+  font-weight: 300;
+  height: 20px;
+  text-align: center;
+  margin-top: 10px;
+  font-style: italic;
+  margin-bottom: 30px;
+`;
+const SubWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 768px) {
+    height: 100vh; /* Use vh as a fallback for browsers that do not support Custom Properties */
+    height: calc(var(--vh, 1vh) * 100);
+    margin: 0 auto;
+  }
+`;
+const ImageInContainer = styled.img`
+  object-fit: cover;
+  max-width: 100%;
+
+  @media (max-width: 768px) {
+    margin-top: 0px;
+    height: 200px;
+  }
+`;
+const ImageInContainer1 = styled.img`
+  object-fit: cover;
+  max-width: 100%;
+
+  @media (max-width: 768px) {
+    margin-top: 0px;
+    height: 200px;
+  }
+`;
+
+const AboutMeContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  width: 30vw;
+  font-size: 14px;
+  font-family: "Montserrat", sans-serif;
+  color: black;
+  font-weight: 300;
+  letter-spacing: 1px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #ffffff;
+  transition: 500ms;
+  opacity: 0.97;
+  height: 450px;
+  cursor: zoom-in;
+  &:hover {
+    transition: 500ms;
+    -webkit-box-shadow: -6px 7px 19px -4px #000000;
+    box-shadow: -6px 7px 19px -4px #000000;
+    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+    filter: grayscale(100%);
+  }
+  @media (max-width: 768px) {
+    width: 90vw;
+    height: auto;
+    padding-bottom: 10px;
+    border: 1px solid gray;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  @media (max-width: 768px) {
+    height: 100vh; /* Use vh as a fallback for browsers that do not support Custom Properties */
+    height: calc(var(--vh, 1vh) * 100);
+    margin: 0 auto;
+  }
+`;
+
+export default GalleryPage;

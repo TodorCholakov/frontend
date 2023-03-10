8eec72e50e857cc2907e018595e180fd82220759
Todor Cholakov <t.s.cholakov@gmail.com>
2023-03-10 12:51:15 +0200

    title added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import { Provider, createClient } from 'urql'
+import Head from 'next/head'
 
 const client = createClient({url: process.env.NEXT_PUBLIC_BACKEND_API})
 
 export default function App({ Component, pageProps }) {
-  return <Provider value={client}><Component {...pageProps} /></Provider>
+  return <Provider value={client}><Component {...pageProps} /><Head>
+    <title>Todor Cholakov - nature photography</title>
+    </Head></Provider>
 }

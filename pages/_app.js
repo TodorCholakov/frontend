1eb8810f3a31c87dfcfd001f1a14568af7021056
Todor Cholakov <t.s.cholakov@gmail.com>
2023-03-13 23:58:10 +0200

    analitucs

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,6 @@ const client = createClient({url: process.env.NEXT_PUBLIC_BACKEND_API})
 export default function App({ Component, pageProps }) {
   return <Provider value={client}><Component {...pageProps} /><Head>
     <title>Todor Cholakov - nature photography</title>
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-W6EW482D4P"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-W6EW482D4P');
-</script>
+    
     </Head></Provider>
 }
